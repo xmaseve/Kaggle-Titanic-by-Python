38918b36b90d54f983fda8ab0fa1eea810812e02
Yi Qi <qiyi1224@gmail.com>
2016-08-02 14:30:40 -0400

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -73,7 +73,6 @@ y = Y.reshape((m,1))
 
 #Test dataset
 test = pd.read_csv('C:/Users/YI/Downloads/test.csv')
-passengerid = test.PassengerId
 test.info()
 test.describe()
 test = test.set_index('PassengerId')
@@ -87,7 +86,7 @@ a, b = np.shape(new1)
 x_test = np.ones((a,b+1))
 x_test[:, 1:] = new1
 
-passengerid = test.PassengerID
+
 
 ##Logistic regression
 #sigmoid function
@@ -95,14 +94,6 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 
-#loss function
-theta = np.ones((n+1, 1))
-def loss(x, y, theta):
-    z = x.dot(theta)
-    L = -y.T.dot(np.log(sigmoid(z)))-(1-y).T.dot(np.log(1 - sigmoid(z)))
-    return L
-   
-loss(x, y, theta) 
 
 #batch gradient ascend    
 def gradient(x, y, num_iters=200):
@@ -129,7 +120,6 @@ def sga(x, y, num_iters=500):
             theta = theta + alpha * (y[randindex] - h) * x[randindex]
     return theta
     
-theta = sga(x, y)
 
 #Generate predicted values
 def classify(x, theta):
@@ -143,6 +133,7 @@ def classify(x, theta):
             result.append(0)
     return result
 
+theta = sga(x, y)
 result = classify(x_test, theta)
 
 presult=pd.DataFrame(result, columns=['Survived'])
@@ -152,24 +143,7 @@ submission.to_csv('C:/Users/YI/Downloads/Titanic_submission.csv')
 
 
 
-def classify(x, theta):
-    prob = sigmoid(np.sum(x * theta))
-    if prob >= 0.5:
-        return 1
-    else:
-        return 0
-        
-        
-def testerror(x, y):
-    m, n = np.shape(newtest)
-    errorcount = 0
-    theta = sga(x, y, 1000)
-    for i in range(m):
-        if classify(x[i], theta) != y[i]:
-            errorcount += 1
-    accuracy = 1 - float(errorcount) / m
-    return accuracy
-    
+
 
 
 
@@ -190,110 +164,6 @@ def entropy(x):
     return entropy
     
 
-##PCA
-def pca(x, k):
-    avg = np.mean(x, axis=0)
-    x = x - avg
-    covx = np.cov(x, rowvar=0)
-    eigvalue, eigvec = np.linalg.eig(covx)
-    valindex = np.argsort(eigvalue)
-    valindex = valindex[:-(valindex+1):-1]
-    redeigvec = eigvec[:, valindex]
-    lowx = x * redeigvec
-    reconx =  lowx * redeigvec.T + x
-    return reconx
     
 
 
-
-
-
-########################################################################
-for i in b:
-    for j in range(3):
-        cluster=[]
-        if j == i:
-            cluster.append(j)
-        print cluster
-            
-def f():
-    return 2
-
-a=f()
-
-def f(x):
-    a = []
-    while x > 0:
-        a.append(x)
-        f(x-1)
-
-def Square(x):
-    return SquareHelper(abs(x), abs(x))
-
-def SquareHelper(n, x):
-    if n == 0:
-        return 0
-    return SquareHelper(n-1, x) + x
-
-def isPalindrome(aString):
-    m = len(aString)
-    l = []
-    for i in range(m):
-        if aString[i] == aString[m-i-1]:
-            l.append(1)
-        else:
-            l.append(0)
-
-    if 0 not in l:
-        return True
-    else:
-        return False
-
-
-a = [1,2,3]
-b = [3,4,5]
-
-l = []
-for i,j in zip(a,b):
-    l.append( i * j)
-print sum(l)
-
-def flatten(aList):
-    l =[]
-    for i in aList:
-        
-        if type(i) == 'str':
-            l.append(i)
-        elif type(i) == 'int':
-            l.append(i)
-    if type(i) == 'list':
-        l.extend(flatten(i))
-    
-    return l
-
-h=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
-
-def flatten(lst):
-	return sum( ([x] if not isinstance(x, list) else flatten(x)
-		     for x in lst), [] ):
-           
-           
-def dict_interdiff(d1, d2):
-    # symmetric difference, keys in either d1 or d2 but not both.
-    lst = []
-    sym_diff = d1.viewkeys() ^ d2
-    # intersection, keys that are common to both d1 and d2.
-    intersect = d1.viewkeys() & d2
-    # apply f on values of the keys that common to both dicts.
-    a = {k: f(d1[k], d2[k]) for k in intersect}
-    b = {k: d1[k] for k in sym_diff & d1.viewkeys()}
-    # add key/value pairings from d2 using keys that appear in sym_diff 
-    b.update({k: d2[k] for k in sym_diff & d2.viewkeys()})
-    lst.append(a)
-    lst.append(b)
-    c = tuple(lst)
-    return c
-                
-                
-                
-         
